d651d17efa22319f699c8dd24b997c5273ff0b29
Jake Hemmerle <jakehemmerle@protonmail.com>
2022-03-19 18:11:51 -0400

    cleaned up a bit and fixed compilation issues

--- pallets/uc-qf/src/lib.rs
+++ pallets/uc-qf/src/lib.rs
@@ -16,7 +16,19 @@ pub use frame_support::sp_runtime::{traits::{AccountIdConversion, Saturating, Ze
 pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
+    use frame_support::BoundedVec;
+    use frame_support::traits::ConstU32;
 
+    // ProjectID used to uniquely identify a project
+    pub type ProjectID = u32;
+
+    pub type RoundID = u32;
+    #[derive(Encode, Decode, Default, PartialEq, Eq, TypeInfo, MaxEncodedLen)]
+    pub struct ProjectDetails<AccountId> {
+        name: BoundedVec<u8, ConstU32<32>>,
+        owner: AccountId,
+        payment_destination: AccountId
+    }
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
@@ -31,43 +43,36 @@ pub mod pallet {
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
-    #[derive(Encode, Decode, Default, PartialEq, Eq, TypeInfo, MaxEncodedLen)]
-    pub struct ProjectDetails<AccountId> {
-        owner: AccountId,
-        destination: AccountId
-    }
-
-    type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
-    type ProjectDetailsOf<T> = ProjectDetails<AccountIdOf<T>>;
-
-    // ID generation storage items
-    // keeping track of the next id to distribute
     #[pallet::type_value]
-    pub fn DefaultID<T: Config>() -> u32 { 1 }
+    pub fn DefaultID() -> u32 { 1 }
+
     #[pallet::storage]
-    pub type RoundID<T> = StorageValue<_, u32, ValueQuery, DefaultID<T>>;
+    #[pallet::getter(fn next_round_id)]
+    pub type NextRoundID<T: Config> = StorageValue<_, RoundID, ValueQuery, DefaultID>;
+
     #[pallet::storage]
-    pub type ProjectID<T> = StorageValue<_, u32, ValueQuery, DefaultID<T>>;
+    #[pallet::getter(fn next_project_id)]
+    pub type NextProjectID<T: Config> = StorageValue<_, ProjectID, ValueQuery, DefaultID>;
 
     // (coordinator_account_id) -> (round_id)
     // doing it this way so the coordinator can call functions without tracking the round_id,
     // only the users making a vote need to track the round_id
     // also prevents outside manipulation (i.e., non-coordinator ending voting, non-coordinator adding new project)
     #[pallet::storage]
-    pub type Coordinator<T> = StorageMap<_, Identity, AccountIdOf<T>, u32>;
+    pub type Coordinator<T: Config> = StorageMap<_, Identity, <T as frame_system::Config>::AccountId, u32>;
 
     // (round_id) -> (is_active)
     // track if a specific round is active or not
     #[pallet::storage]
-    pub type Round<T> = StorageMap<_, Identity, u32, bool>;
+    pub type Round<T: Config> = StorageMap<_, Identity, RoundID, bool>;
 
     // (project_id) -> (project details)
     #[pallet::storage]
-    pub type Project<T: Config> = StorageMap<_, Blake2_128Concat, u32, ProjectDetailsOf<T>, OptionQuery>;
+    pub type Project<T: Config> = StorageMap<_, Identity, ProjectID, ProjectDetails<T::AccountId>, OptionQuery>;
 
     // (project_owner_account_id) -> (project_id)
     #[pallet::storage]
-    pub type ProjectOwner<T> = StorageMap<_, Identity, AccountIdOf<T>, u32>;
+    pub type ProjectOwner<T: Config> = StorageMap<_, Identity, <T as frame_system::Config>::AccountId, ProjectID>;
 
     // (round_id) -> [(project_id)]
     #[pallet::storage]
@@ -77,12 +82,12 @@ pub mod pallet {
     // benefit of doing this is when a user votes more than once on a single project, only their most recent vote will be counted
     // and with an NMap we can iterate on a partial key (i.e., just the round and project IDs, so we can get each vote in batches by project_id)
     #[pallet::storage]
-    pub type Vote<T> = StorageNMap<
+    pub type Vote<T: Config> = StorageNMap<
         _,
         (
-            NMapKey<Blake2_128Concat, u32>,
-            NMapKey<Blake2_128Concat, u32>,
-            NMapKey<Blake2_128Concat, AccountIdOf<T>>
+            NMapKey<Blake2_128Concat, RoundID>,
+            NMapKey<Blake2_128Concat, ProjectID>,
+            NMapKey<Blake2_128Concat, T::AccountId>
         ),
         u32,
         ValueQuery
@@ -93,15 +98,17 @@ pub mod pallet {
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-        NewVotingRound(u32),
-        NewProjectCreated(u32),
-        ProjectRegistered(u32, u32),
+        NewVotingRound(RoundID),
+        NewProjectCreated(ProjectID),
+        ProjectRegistered(RoundID, ProjectID),
 	}
 
 	#[pallet::error]
 	pub enum Error<T> {
         InvalidRoundID,
+        OverflowRoundID,
         InvalidProjectID,
+        
         ProjectAlreadyRegistered,
         ProjectNotRegistered,
         MaximumProjectsReached,
@@ -140,11 +147,11 @@ pub mod pallet {
         // PROJECT OWNER FUNCTION
         // the user who calls create_new_project will become the Project Owner
         #[pallet::weight(100)]
-        pub fn create_new_project(origin: OriginFor<T>, destination_account: T::AccountId) -> DispatchResult {
+        pub fn create_new_project(origin: OriginFor<T>, name: BoundedVec<u8, ConstU32<32>>, payment_destination: T::AccountId) -> DispatchResult {
             let who = ensure_signed(origin)?;
 
             let project_id = Self::get_new_project_id();
-            let project_details = ProjectDetails { owner: who, destination: destination_account };
+            let project_details = ProjectDetails { owner: who, name, payment_destination };
             <Project<T>>::insert(project_id, project_details);
 
             Self::deposit_event(Event::NewProjectCreated(project_id));
@@ -253,16 +260,18 @@ pub mod pallet {
 
     // Private Functions
     impl<T: Config> Pallet<T> {
-        fn get_new_round_id() -> u32 {
-            let round_id = RoundID::<T>::get();
-            RoundID::<T>::put(round_id.wrapping_add(1));
-            return round_id;
+        fn get_new_round_id() -> RoundID {
+            let round_id = Self::next_round_id();
+            let next_round_id: RoundID = round_id.wrapping_add(1);
+            NextRoundID::<T>::put(next_round_id);
+            round_id
         }
 
-        fn get_new_project_id() -> u32 {
-            let project_id = ProjectID::<T>::get();
-            ProjectID::<T>::put(project_id.wrapping_add(1));
-            return project_id;
+        fn get_new_project_id() -> ProjectID {
+            let project_id = Self::next_project_id();
+            let next_project_id: ProjectID = project_id.wrapping_add(1);
+            NextProjectID::<T>::put(next_project_id);
+            project_id
         }
 
         // Check if a given coordinator is associated with an active round
@@ -295,7 +304,7 @@ pub mod pallet {
         }
 
         // distribute funds into project accounts
-        fn perform_qf_and_distribute_funds(round_id: u32) {
+        fn perform_qf_and_distribute_funds(round_id: RoundID) {
             // get list of projects for this round_id
             let mut projects = vec![];
             match <Projects<T>>::try_get(round_id) {
